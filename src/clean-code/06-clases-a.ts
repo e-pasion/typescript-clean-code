e0267259380d8835b4bad0d80a26e1f3edd4e1cf
elias pasion <elias.pasion@pragma.com.co>
2024-03-09 21:04:55 -0500

    exercise 6b

--- src/clean-code/06-clases-a.ts
+++ src/clean-code/06-clases-a.ts
@@ -1,6 +1,6 @@
 (() => {
   type Gender = "M" | "F";
-
+  //No aplicnado le princcipio de responsabilidad unica
   class Person {
     constructor(
       public name: string,
@@ -9,6 +9,47 @@
     ) {}
   }
 
-  const newPerson: Person = new Person("Orlando", "M", new Date("2003-06-11"));
-  console.log({ newPerson });
+  class User extends Person {
+    public lastAccess: Date;
+    constructor(
+      public email: string,
+      public role: string,
+      name: string,
+      gender: Gender,
+      birthdate: Date
+    ) {
+      super(name, gender, birthdate);
+      this.lastAccess = new Date();
+    }
+
+    checkCredentials() {
+      return true;
+    }
+  }
+
+  class UserSettings extends User {
+    constructor(
+      public workingDirectory: string,
+      public lastOpenFolder: string,
+      email: string,
+      role: string,
+      name: string,
+      gender: Gender,
+      birthdate: Date
+    ) {
+      super(email, role, name, gender, birthdate);
+    }
+  }
+
+  const userSettings= new UserSettings(
+    '/usr/home',
+    '/home',
+    'elkin@gmail.com',
+    'Admin',
+    'Elkin',
+    'M',
+    new Date('2003-06-11')
+  );
+
+  console.log({userSettings, areCredentialsValid: userSettings.checkCredentials()})
 })();
